acc182c0a23480b45416729c5a669dc08aaca04f
Kasra Sanati <kasrasanati77@gmail.com>
2024-04-26 15:53:24 -0400

    my first commit for testing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function LandingPage() {
   return (
     <div>
-      <h1>Welcome to our website!</h1>
-      {/* Add content and styling for the landing page */}
+          <h1>Welcome to our website!</h1>
+      <h1>SamQ your test friend</h1>
+          {/* Add content and styling for the landing page */}
     </div>
   );
 }
