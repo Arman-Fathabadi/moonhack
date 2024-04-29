165492c9325615aba815073f67edb482d2084551
Arman <armanfathabadi99@gmail.com>
2024-04-29 11:59:54 -0400

    Added Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React from 'react';
 import "../styles/Contact.css";
 
 function Contact() {
   return (
     <div>
-      <h1>Contact Us</h1>
-      {/* Add contact form and information */}
+      {/* ... other contact information ... */}
+      {/* LinkedIn link */}
+      <div>
+        <a href="https://www.linkedin.com/in/arman-fathabadi/">
+          Arman's LinkedIn
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/ahmadreza-azizi-/">
+          Ahmadreza's LinkedIn
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/kasra-sanati-80a5432b8/">
+          Kasra's LinkedIn
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/mahdiar-khodabakhshi/">
+          Mahdiar's LinkedIn
+        </a>
+      </div>
     </div>
   );
 }
