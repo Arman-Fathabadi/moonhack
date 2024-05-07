dc6d1fb019a63468c556a5f96723022f489874c4
Kasra Sanati <kasrasanati77@gmail.com>
2024-05-06 23:56:28 -0400

    about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,10 +3,35 @@ import "../styles/AboutUs.css";
 
 function AboutUs() {
   return (
-    <div id="about">
-      <h1>About Us</h1>
-      <p>we made samQ to help us on exams practice makes perfect</p>
-    </div>
+    <><div class="bg-primary h-12 flex justify-center items-center">
+          <h1 class="text-light font-bold text-2xl">About Us</h1>
+      </div><div class="bg-light p-4">
+              <div class="mt-8">
+                  <h2 class="text-primary font-bold text-2xl">Our Mission</h2>
+                  <p class="text-gray-500 font-medium text-lg">
+                      Here is your station for knowledge, with us you can find your way through these mysterious issues and create a solid base!As this site is not just an package of answers; rather, it is a living lab designed based on your exact demands and needs. Are you struggling with Mathematics, puzzling with Physics, or, in any other case, facing a complicating question find it hard to find the reliability the essential guiding help? You can count on us.
+                      Just type your questions, and our super-powered system will be spurred to do all the work. And there, it goes through word-by-word careful scrutiny, noticing the underlying topic area, either simple or complicated, and the desired type of guidance - be it. From there, we'll move to a deep search in our whole library where we'll grab the most important sample questions with proper explanation at the back end of the search.
+                      Go ahead and say to yourself: these are cases from my very own textbook. Those examples will point out highlight how different problem solving strategies will fit, broaden your knowledge and make you feel more confident when you'll face similar situations. Come on, guys! Let's do this together and convert those question marks to spots of triumph and learning. 
+                  </p>
+              </div>
+              <div class="mt-8">
+                  <h2 class="text-primary font-bold text-2xl">Our Team</h2>
+                  <ul class="list-disc ml-4">
+                      <li class="text-gray-500 font-medium text-lg">
+                          Ahmadreza Azizi
+                      </li>
+                      <li class="text-gray-500 font-medium text-lg">
+                          Mahdiar Khodabakhshi
+                      </li>
+                      <li class="text-gray-500 font-medium text-lg">
+                          Arman Fathabadi
+                      </li>
+                      <li class="text-gray-500 font-medium text-lg">
+                          Kasra Sanati
+                      </li>
+                  </ul>
+              </div>
+          </div></>
   );
 }
 
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -2,19 +2,8 @@ import React from "react";
 import "./infoStyle.css";
 
 function Info() {
-  return (
-    <div className="info-container">
-      <h2>Information</h2>
-      <div className="info-section">
-        <h3>Section 1 Title</h3>
-        <p>Information related to section 1...</p>
-      </div>
-      <div className="info-section">
-        <h3>Section 2 Title</h3>
-        <p>Information related to section 2...</p>
-      </div>
-      {/* It may need to change/add more content */}
-    </div>
+    return (
+        
   );
 }
 
