b3af009fd4a4a26ade01335a06203e9cbdfdbd38
Kasra Sanati <kasrasanati77@gmail.com>
2024-05-06 21:29:51 -0400

    Revert "Added Contact"
    This reverts commit 165492c9325615aba815073f67edb482d2084551.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,31 +1,11 @@
-import React from 'react';
+import React from "react";
 import "../styles/Contact.css";
 
 function Contact() {
   return (
     <div>
-      {/* ... other contact information ... */}
-      {/* LinkedIn link */}
-      <div>
-        <a href="https://www.linkedin.com/in/arman-fathabadi/">
-          Arman's LinkedIn
-        </a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/ahmadreza-azizi-/">
-          Ahmadreza's LinkedIn
-        </a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/kasra-sanati-80a5432b8/">
-          Kasra's LinkedIn
-        </a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/mahdiar-khodabakhshi/">
-          Mahdiar's LinkedIn
-        </a>
-      </div>
+      <h1>Contact Us</h1>
+      {/* Add contact form and information */}
     </div>
   );
 }
