95e7f78258f61b1f1188b1b6e1d7cff99c6ab622
Kasra Sanati <kasrasanati77@gmail.com>
2024-05-06 23:26:46 -0400

    about us changed

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,9 +3,9 @@ import "../styles/AboutUs.css";
 
 function AboutUs() {
   return (
-    <div>
+    <div id="about">
       <h1>About Us</h1>
-      {/* Add content and styling for the about us page */}
+      <p>we made samQ to help us on exams practice makes perfect</p>
     </div>
   );
 }
